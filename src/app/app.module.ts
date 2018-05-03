eea16a72dc8290825bc2889d786926ddbf30b7ef
u0090265 <tom.dedobbeleer@kuleuven.be>
2018-05-03 15:38:35 +0200

    update to latest angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import {BrowserModule} from "@angular/platform-browser";
 import {NgModule} from "@angular/core";
 import {FormsModule} from "@angular/forms";
-import {HttpModule} from "@angular/http";
 import {AppComponent} from "./app.component";
 import {TranslationService} from "./services/translation.service";
 import {TranslationPipe} from "./pipes/translation.pipe";
@@ -14,7 +13,6 @@ import {TranslationPipe} from "./pipes/translation.pipe";
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
   ],
   providers: [TranslationService],
   bootstrap: [AppComponent]
