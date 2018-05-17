f358cb69c1ca12473a90131bc5faaba050395786
u0090265 <tom.dedobbeleer@kuleuven.be>
2018-05-17 13:39:05 +0200

    add opeing hours / smaller map for small devices

--- src/app/lang/lang-es.ts
+++ src/app/lang/lang-es.ts
@@ -14,4 +14,13 @@ export const LANG_ES_TRANS = {
   'text.food': 'In hac habitasse platea dictumst. Etiam at tempor justo. Morbi convallis lectus nunc, a suscipit dolor accumsan non. Maecenas sed ante a mauris suscipit consequat ac ac nisl. Quisque dictum risus lacus, sit amet rhoncus diam tincidunt vel. Curabitur justo neque, congue ut neque non, ornare tristique dolor. Cras congue imperdiet sapien sed eleifend. Nulla bibendum mattis tellus, eget ornare est blandit eu. Duis et dui porttitor, accumsan nisl egestas, consequat neque. Nullam non dui venenatis, dictum magna ut, sagittis leo. Sed convallis auctor dui, vitae lobortis sem luctus vitae. Fusce et mattis quam.',
   'title.about': "Sobre nosotros",
   'text.about': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed interdum magna, sed maximus sapien. Vivamus elementum, turpis et scelerisque condimentum, leo diam vulputate urna, vel molestie nunc sapien vel tortor. Integer porta non quam id porttitor. Suspendisse non magna consequat dolor laoreet interdum in quis tortor. Aliquam ultrices interdum enim nec euismod. Quisque hendrerit auctor iaculis. Ut maximus, elit non volutpat bibendum, quam nulla rutrum justo, sed fermentum metus lacus lobortis metus. Integer vehicula eros a diam tempor dapibus. Pellentesque ut justo volutpat, condimentum nulla eget, malesuada arcu. Vivamus cursus ex sed nunc facilisis consequat. Cras tincidunt mattis scelerisque. Maecenas pulvinar ante sit amet velit interdum tincidunt. Integer congue sed nulla nec ultricies. Nulla consectetur cursus tincidunt. Integer tortor nisl, euismod eget faucibus sit amet, ultrices et neque.',
+  'text.monday': "Lunes",
+  'text.tuesday': "Martes",
+  'text.wednesday': "Miércoles",
+  'text.thursday': "Jueves",
+  'text.friday': "Viernes",
+  'text.saturday': "Sábado",
+  'text.sunday': "Domingo",
+  'text.closed': 'Cerrado',
+  'text.opening.hours': 'Horas de apertura',
 };
