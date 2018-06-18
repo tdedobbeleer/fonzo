49dc86947c6b3f517fa039f31b53b5136adab534
u0090265 <tom.dedobbeleer@kuleuven.be>
2018-06-18 20:33:17 +0200

    text + booze menu

--- src/app/lang/lang-nl.ts
+++ src/app/lang/lang-nl.ts
@@ -10,9 +10,17 @@ export const LANG_NL_TRANS = {
   'nav.about': 'Over ons',
   'nav.contact': 'Contacteer ons',
   'title.food': 'Eten en drinken',
-  'text.food': 'In hac habitasse platea dictumst. Etiam at tempor justo. Morbi convallis lectus nunc, a suscipit dolor accumsan non. Maecenas sed ante a mauris suscipit consequat ac ac nisl. Quisque dictum risus lacus, sit amet rhoncus diam tincidunt vel. Curabitur justo neque, congue ut neque non, ornare tristique dolor. Cras congue imperdiet sapien sed eleifend. Nulla bibendum mattis tellus, eget ornare est blandit eu. Duis et dui porttitor, accumsan nisl egestas, consequat neque. Nullam non dui venenatis, dictum magna ut, sagittis leo. Sed convallis auctor dui, vitae lobortis sem luctus vitae. Fusce et mattis quam.',
+  'text.food': 'In onze bar kunnen jullie genieten van onze heerlijke en originele huisgemaakte gerechten. We\n' +
+  'proberen uitsluitend te werken met verse producten. Daarnaast hebben we\n' +
+  'ook een mooie selectie aan wijnen, vermouts, artisanale bieren, gins en likeuren. Kom gerust eens langs voor een hapje en een drankje en vraag naar de Nederlandstalige menukaart!\n<br/><br/>' +
+  'Onze drankkaart kan je alvast <a href="/assets/files/bebidas.pdf" download>hier downloaden</a>',
   'title.about': "Over ons",
-  'text.about': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed interdum magna, sed maximus sapien. Vivamus elementum, turpis et scelerisque condimentum, leo diam vulputate urna, vel molestie nunc sapien vel tortor. Integer porta non quam id porttitor. Suspendisse non magna consequat dolor laoreet interdum in quis tortor. Aliquam ultrices interdum enim nec euismod. Quisque hendrerit auctor iaculis. Ut maximus, elit non volutpat bibendum, quam nulla rutrum justo, sed fermentum metus lacus lobortis metus. Integer vehicula eros a diam tempor dapibus. Pellentesque ut justo volutpat, condimentum nulla eget, malesuada arcu. Vivamus cursus ex sed nunc facilisis consequat. Cras tincidunt mattis scelerisque. Maecenas pulvinar ante sit amet velit interdum tincidunt. Integer congue sed nulla nec ultricies. Nulla consectetur cursus tincidunt. Integer tortor nisl, euismod eget faucibus sit amet, ultrices et neque.',
+  'text.about': 'Hartelijk welkom op onze website! Wij zijn Julie Impens en Pieter De Dobbeleer, uitbaters van Fonzo.\n' +
+  'Al jaren zijn we gepassioneerd door lekker en gezond eten en we willen dat iedereen kan genieten\n' +
+  'aan een betaalbare prijs.<br/><br/>' +
+  'Fonzo is een kleurrijke, eigenzinnige, verrassende ruimte vol snuisterijen waar er telkens iets te\n' +
+  'ontdekken valt. De naam van de bar is een ode aan onze te jong gestorven vader (Fons De Dobbeleer) - Hij waakt nog\n' +
+  'steeds over de bar met zijn foto aan de ingang.',
   'text.monday': "Maandag",
   'text.tuesday': "Dinsdag",
   'text.wednesday': "Woensdag",
