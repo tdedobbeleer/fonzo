09aa51207a8bf197093a8f65af5a7f2a7174399b
u0090265 <tom.dedobbeleer@kuleuven.be>
2018-06-19 09:25:36 +0200

    added english and spanish translations

--- src/app/lang/lang-nl.ts
+++ src/app/lang/lang-nl.ts
@@ -10,17 +10,9 @@ export const LANG_NL_TRANS = {
   'nav.about': 'Over ons',
   'nav.contact': 'Contacteer ons',
   'title.food': 'Eten en drinken',
-  'text.food': 'In onze bar kunnen jullie genieten van onze heerlijke en originele huisgemaakte gerechten. We\n' +
-  'proberen uitsluitend te werken met verse producten. Daarnaast hebben we\n' +
-  'ook een mooie selectie aan wijnen, vermouts, artisanale bieren, gins en likeuren. Kom gerust eens langs voor een hapje en een drankje en vraag naar de Nederlandstalige menukaart!\n<br/><br/>' +
-  'Onze drankkaart kan je alvast <a href="/assets/files/bebidas.pdf" download>hier downloaden</a>',
+  'text.food': 'In onze bar kunnen jullie genieten van onze heerlijke en originele huisgemaakte gerechten. We proberen uitsluitend te werken met verse producten. Daarnaast hebben we ook een mooie selectie aan wijnen, vermouts, artisanale bieren, gins en likeuren. Kom gerust eens langs voor een hapje en een drankje en vraag naar de Nederlandstalige menukaart!\n<br/><br/>Onze drankkaart kan je alvast <a href="/assets/files/bebidas.pdf" download>hier downloaden</a>',
   'title.about': "Over ons",
-  'text.about': 'Hartelijk welkom op onze website! Wij zijn Julie Impens en Pieter De Dobbeleer, uitbaters van Fonzo.\n' +
-  'Al jaren zijn we gepassioneerd door lekker en gezond eten en we willen dat iedereen kan genieten\n' +
-  'aan een betaalbare prijs.<br/><br/>' +
-  'Fonzo is een kleurrijke, eigenzinnige, verrassende ruimte vol snuisterijen waar er telkens iets te\n' +
-  'ontdekken valt. De naam van de bar is een ode aan onze te jong gestorven (schoon)vader Fons De Dobbeleer - Hij waakt nog\n' +
-  'steeds over de bar met zijn foto aan de ingang.',
+  'text.about': 'Hartelijk welkom op onze website! Wij zijn Julie Impens en Pieter De Dobbeleer, uitbaters van Fonzo. Al jaren zijn we gepassioneerd door lekker en gezond eten en we willen dat iedereen kan genieten aan een betaalbare prijs.<br/><br/>Fonzo is een kleurrijke, eigenzinnige, verrassende ruimte vol snuisterijen waar er telkens iets te ontdekken valt. De naam van de bar is een ode aan onze te jong gestorven (schoon)vader Fons De Dobbeleer - Hij waakt nog steeds over de bar met zijn foto aan de ingang.',
   'text.monday': "Maandag",
   'text.tuesday': "Dinsdag",
   'text.wednesday': "Woensdag",
