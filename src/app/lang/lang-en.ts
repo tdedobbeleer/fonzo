a57c4db67435c8839ab71c075dbe1fe91cc71b55
tdedobbeleer <tdedobbeleer@gmail.com>
2018-06-27 21:32:58 +0200

    Drink -> drinks

--- src/app/lang/lang-en.ts
+++ src/app/lang/lang-en.ts
@@ -10,7 +10,7 @@ export const LANG_EN_TRANS = {
   'nav.about': 'About us',
   'nav.contact': 'Contact',
   'title.food': 'Food and drinks',
-  'text.food': 'In our bar you can enjoy our delicious and original home-made dishes. We try to work exclusively with fresh products. We also have a nice selection of wines, vermouths, artisanal beers, gins and liqueurs. Feel free to drop by for a drink or a bite to eat!<br/> <br/> Our drink menu can already be downloaded <a href="/assets/files/menu_fonzo.pdf" download> here</a>.',
+  'text.food': 'In our bar you can enjoy our delicious and original home-made dishes. We try to work exclusively with fresh products. We also have a nice selection of wines, vermouths, artisanal beers, gins and liqueurs. Feel free to drop by for a drink or a bite to eat!<br/> <br/> Our drinks menu can already be downloaded <a href="/assets/files/menu_fonzo.pdf" download> here</a>.',
   'title.about': "About us",
   'text.about': 'Welcome to our website! We are Julie Impens and Pieter De Dobbeleer, owners of Fonzo. For years we have been passionate about delicious and healthy food we want everyone to enjoy at an affordable price. <br/> <br/> Fonzo is a colorful, quirky, surprising space full of trinkets where there is always something to discover. The name of the bar is an ode to our father(-in-law) Fons De Dobbeleer, who died too young. He still watches over the bar with his photo at the entrance.',
   'text.monday': "Monday",
