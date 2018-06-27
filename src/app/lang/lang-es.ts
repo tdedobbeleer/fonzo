8d45c5e68af51d501b0e2a62ddb81fa354ecbfda
u0090265 <tom.dedobbeleer@kuleuven.be>
2018-06-27 13:55:29 +0200

    new card

--- src/app/lang/lang-es.ts
+++ src/app/lang/lang-es.ts
@@ -11,7 +11,7 @@ export const LANG_ES_TRANS = {
   'nav.about': 'Sobre nosotros',
   'nav.contact': 'Contacto',
   'title.food': 'Bebidas y tapas',
-  'text.food': 'En nuestro bar podrá disfrutar de nuestros deliciosos y originales platos caseros. Intentamos trabajar exclusivamente con productos frescos. También tenemos una buena selección de vinos, vermuts, cervezas artesanales, ginebras y licores. No dude en venir a tomar un aperitivo y una bebida<br /> <br/> Nuestra tarjeta de bebida ya se puede descargar <a href="/assets/files/bebidas.pdf" download> aquí</a>.',
+  'text.food': 'En nuestro bar podrá disfrutar de nuestros deliciosos y originales platos caseros. Intentamos trabajar exclusivamente con productos frescos. También tenemos una buena selección de vinos, vermuts, cervezas artesanales, ginebras y licores. No dude en venir a tomar un aperitivo y una bebida<br /> <br/> Nuestra tarjeta de bebida ya se puede descargar <a href="/assets/files/menu_fonzo.pdf" download> aquí</a>.',
   'title.about': "Sobre nosotros",
   'text.about': '¡Bienvenido a nuestro sitio web! Somos Julie Impens y Pieter De Dobbeleer, dueños de Fonzo. Durante años hemos sido apasionados por la comida deliciosa y saludable y queremos que todos disfruten de un precio asequible. <br/> <br/> Fonzo es un espacio colorido, extravagante, sorprendente lleno de baratijas donde siempre hay algo por descubrir. El nombre del bar es una oda a nuestro padre/suegro Fons De Dobbeleer, que murió demasiado joven. Todavía vigila la barra con su foto en la entrada.',
   'text.monday': "Lunes",
