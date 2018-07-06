61980cc589a958f89864da7d8bcd49b3076e2998
u0090265 <tom.dedobbeleer@kuleuven.be>
2018-07-06 10:34:38 +0200

    sitemap update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,8 +15,17 @@ const appRoutes: Routes = [
   {path: 'en', redirectTo: '/?lang=en'},
   {path: 'es', redirectTo: '/?lang=es'},
   {path: 'contact', redirectTo: '/#contact'},
+  {path: 'en/contact', redirectTo: '/?lang=en#contact'},
+  {path: 'nl/contact', redirectTo: '/?lang=nl#contact'},
+  {path: 'es/contact', redirectTo: '/?lang=es#contact'},
   {path: 'about', redirectTo: '/#about'},
-  {path: 'menu', redirectTo: '/#menu'}
+  {path: 'en/about', redirectTo: '/?lang=en#about'},
+  {path: 'nl/about', redirectTo: '/?lang=nl#about'},
+  {path: 'es/about', redirectTo: '/?lang=es#about'},
+  {path: 'food', redirectTo: '/#food'},
+  {path: 'en/food', redirectTo: '/?lang=en#food'},
+  {path: 'nl/food', redirectTo: '/?lang=nl#food'},
+  {path: 'es/food', redirectTo: '/?lang=es#food'},
 ];
 
 @NgModule({
