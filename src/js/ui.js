2f654f93f2f241c60ddd68db4e18a2e79bec9a67
tdedobbeleer <tdedobbeleer@gmail.com>
2020-08-20 20:04:31 +0200

    change id for menu

--- src/js/ui.js
+++ src/js/ui.js
@@ -105,7 +105,7 @@
         'facebook': 'https://www.facebook.com/fonzomalaga/',
         'instagram': 'https://www.instagram.com/fonzomalaga/',
         'map': 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3197.9215589477885!2d-4.42244498471226!3d36.724445079964326!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd72f7bf07bccf93%3A0x1275bcd023df0785!2sCalle+Mariblanca%2C+4%2C+29012+M%C3%A1laga%2C+Spanje!5e0!3m2!1snl!2sbe!4v1526496334857',
-        'download.menu' : 'https://drive.google.com/uc?export=download&id=1h28t7puZXC_GGqLxxShwq8a1fCrQ6dBF',
+        'download.menu' : 'https://drive.google.com/uc?export=download&id=19jJ2Av6VDkMbHgufm-HkYZHIe08WC6va',
         'download.drinks' : 'https://drive.google.com/uc?export=download&id=1GgADTtPWS2h-YSEpk7K2hMma5fY5yaec',
         'carousel-images': [
             'img/carousel_1.jpg',
