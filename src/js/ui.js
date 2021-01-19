7ccd98b40db412392929855941d120ada19c0122
Tom <tdedobbeleer@gmail.com>
2021-01-19 12:22:11 +0100

    restyle attempt

--- src/js/ui.js
+++ src/js/ui.js
@@ -1,19 +1,10 @@
 (function ($, aos) {
     var LANG_EN = {
-        'text.loading': 'Loading...',
-        'text.home.where': 'Where are we located?',
+        'nav.philosophy': 'Philosophy',
+        'nav.book.now': 'Book now',
         'nav.home': 'Home',
-        'nav.food': 'Food and drinks',
-        'nav.about': 'About us',
-        'nav.reservations' : 'Reservations',
+        'nav.menu': 'Menu',
         'nav.contact': 'Contact',
-        'title.reservations' : 'Book your table',
-        'text.reservations.form' : 'Or fill out this very convenient form:',
-        'text.reservations.whatsapp' : 'You can either contact us via <a data-property-href="whatsapp" target="_blank" rel="nofollow" href="">Whatsapp <i class="fab fa-whatsapp"></i></a>.',
-        'title.food': 'Food and drinks',
-        'text.food': 'In our bar you can enjoy our delicious and original home-made dishes. We try to work exclusively with fresh products. We also have a nice selection of wines, vermouths, artisanal beers, gins and liqueurs. Feel free to drop by for a drink or a bite to eat!<br/> <br/> Our drinks menu can already be downloaded <a href="/drinks" target="_blank"> here</a>.',
-        'title.about': "About us",
-        'text.about': 'Welcome to our website! We are Julie Impens and Pieter De Dobbeleer, owners of Fonzo. For years we have been passionate about delicious and healthy food we want everyone to enjoy at an affordable price. <br/> <br/> Fonzo is a colorful, quirky, surprising space full of trinkets where there is always something to discover. The name of the bar is an ode to our father(-in-law) Fons De Dobbeleer, who died too young. He still watches over the bar with his photo at the entrance.',
         'text.monday': "Monday",
         'text.tuesday': "Tuesday",
         'text.wednesday': "Wednesday",
@@ -30,66 +21,6 @@
             {"text" : "It has become one of my favorite places. Very hospitable and a very good service. Ideal to drink a vermouth.", "source" : "María O. via Google", "url" : "https://www.google.com/maps/contrib/116689621246882345984/place/ChIJd_8ghKP3cg0R5x5rXGr9tJE/@44.0876685,0.3440576,5z/data=!4m6!1m5!8m4!1e1!2s116689621246882345984!3m1!1e1?hl=nl-BE"},
             {"text" : "Very picturesque place with a nice variety of rich and healthy flavors. Strongly recommended.", "source" : "Javi C. via Google", "url" : "https://www.google.com/maps/contrib/100582116992476836427/place/ChIJd_8ghKP3cg0R5x5rXGr9tJE/@39.8160002,-6.7148075,7z/data=!4m4!1m3!8m2!1e1!2s100582116992476836427?hl=nl-BE"},
             {"text" : "Small, quirky, very friendly and just a fantastic vibe. The best place to grab a drink and a bite of tapas in Málaga.", "source" : "Patrick R. via Facebook", "url" : "https://www.facebook.com/patrick.redmond01/posts/10156556001002856"}
-        ]
-    };
-    var LANG_NL = {
-        'text.loading': 'Laden...',
-        'text.home.where': 'Waar vind je ons?',
-        'nav.home': 'Home',
-        'nav.food': 'Eten en drinken',
-        'nav.about': 'Over ons',
-        'nav.contact': 'Contacteer ons',
-        'title.food': 'Eten en drinken',
-        'text.food': 'In onze bar kunnen jullie genieten van onze heerlijke en originele huisgemaakte gerechten. We proberen uitsluitend te werken met verse producten. Daarnaast hebben we ook een mooie selectie aan wijnen, vermouts, artisanale bieren, gins en likeuren. Kom gerust eens langs voor een hapje en een drankje en vraag naar de Nederlandstalige menukaart!\n<br/><br/>Onze drankkaart kan je alvast <a href="/drinks" target="_blank">hier downloaden</a>.',
-        'title.about': "Over ons",
-        'text.about': 'Hartelijk welkom op onze website! Wij zijn Julie Impens en Pieter De Dobbeleer, uitbaters van Fonzo. Al jaren zijn we gepassioneerd door lekker en gezond eten en we willen dat iedereen kan genieten aan een betaalbare prijs.<br/><br/>Fonzo is een kleurrijke, eigenzinnige, verrassende ruimte vol snuisterijen waar er telkens iets te ontdekken valt. De naam van de bar is een ode aan onze te jong gestorven (schoon)vader Fons De Dobbeleer - Hij waakt nog steeds over de bar met zijn foto aan de ingang.',
-        'text.monday': "Maandag",
-        'text.tuesday': "Dinsdag",
-        'text.wednesday': "Woensdag",
-        'text.thursday': "Donderdag",
-        'text.friday': "Vrijdag",
-        'text.saturday': "Zaterdag",
-        'text.sunday': "Zondag",
-        'text.closed': 'Gesloten',
-        'text.opening.hours': 'Openingsuren',
-        'text.quotes' : [
-            {"text" : "Wat een leuke plek! Heerlijk eten geproefd in fantastische setting. Voelde aan als een gezellige woonkamer.", "source" : "Ann D. via Facebook", "url" : "https://www.facebook.com/ann.depoorter.927/posts/2060467344206003:0"},
-            {"text" : "The enige manier om te weten waarom we van deze plek houden, is er naartoe gaan en er zelf van te genieten!", "source" : "Agnes D. via Facebook", "url" : "https://www.facebook.com/agnes.deboeck.5/posts/2253377521339779"},
-            {"text" : "Dit is uniek in Málaga. Deze eigenaars zijn zeer gedreven om de beste en origineelste tapas te maken.", "source" : "Moniek V. via Google", "url" : "https://www.google.com/maps/contrib/113732323421234179423/place/ChIJd_8ghKP3cg0R5x5rXGr9tJE/@36.7244451,-4.4202563,17z/data=!4m6!1m5!8m4!1e1!2s113732323421234179423!3m1!1e1?hl=nl-BE"},
-            {"text" : "Het is een van mijn favoriete plekken geworden. Zeer gastvrij en zeer goede service. Ideaal om een ​​vermout te drinken.", "source" : "María O. via Google", "url" : "https://www.google.com/maps/contrib/116689621246882345984/place/ChIJd_8ghKP3cg0R5x5rXGr9tJE/@44.0876685,0.3440576,5z/data=!4m6!1m5!8m4!1e1!2s116689621246882345984!3m1!1e1?hl=nl-BE"},
-            {"text" : " Zeer pittoreske plaats met een zeer goede variëteit aan rijke en gezonde smaken. Sterk aanbevolen.", "source" : "Javi C. via Google", "url" : "https://www.google.com/maps/contrib/100582116992476836427/place/ChIJd_8ghKP3cg0R5x5rXGr9tJE/@39.8160002,-6.7148075,7z/data=!4m4!1m3!8m2!1e1!2s100582116992476836427?hl=nl-BE"},
-            {"text" : "Klein, eigenzinnig en gewoon een fantastische vibe. De beste plek om een hapje en drankje te verkrijgen in Málaga.", "source" : "Patrick R. via Facebook", "url" : "https://www.facebook.com/patrick.redmond01/posts/10156556001002856"}
-        ]
-    };
-
-    var LANG_ES = {
-        'text.loading': 'Loading...',
-        'text.home.where': '¿Dónde estamos?',
-        'text.home.description': 'Something Spanish',
-        'nav.home': 'Home',
-        'nav.food': 'Bebidas y tapas',
-        'nav.about': 'Sobre nosotros',
-        'nav.contact': 'Contacto',
-        'title.food': 'Bebidas y tapas',
-        'text.food': 'En nuestro bar podrá disfrutar de nuestros deliciosos y originales platos caseros. Intentamos trabajar exclusivamente con productos frescos. También tenemos una buena selección de vinos, vermuts, cervezas artesanales, ginebras y licores. No dude en venir a tomar un aperitivo y una bebida<br /> <br/> Nuestra tarjeta de bebida ya se puede descargar <a href="/drinks" target="_blank"> aquí</a>.',
-        'title.about': "Sobre nosotros",
-        'text.about': '¡Bienvenido a nuestro sitio web! Somos Julie Impens y Pieter De Dobbeleer, dueños de Fonzo. Durante años hemos sido apasionados por la comida deliciosa y saludable y queremos que todos disfruten de un precio asequible. <br/> <br/> Fonzo es un espacio colorido, extravagante, sorprendente lleno de baratijas donde siempre hay algo por descubrir. El nombre del bar es una oda a nuestro padre/suegro Fons De Dobbeleer, que murió demasiado joven. Todavía vigila la barra con su foto en la entrada.',
-        'text.monday': "Lunes",
-        'text.tuesday': "Martes",
-        'text.wednesday': "Miércoles",
-        'text.thursday': "Jueves",
-        'text.friday': "Viernes",
-        'text.saturday': "Sábado",
-        'text.sunday': "Domingo",
-        'text.closed': 'Cerrado',
-        'text.opening.hours': 'Horas de apertura',
-        'text.quotes' : [
-            {"text" : "Que lindo lugar Sabía deliciosa comida, excelente ambiente, se sentía como estar en un acogedor salón.", "source" : "Ann D. via Facebook", "url" : "https://www.facebook.com/ann.depoorter.927/posts/2060467344206003:0"},
-            {"text" : "¡La única manera de saber por qué amamos este lugar es ir allí y disfrutar!", "source" : "Agnes D. via Facebook", "url" : "https://www.facebook.com/agnes.deboeck.5/posts/2253377521339779"},
-            {"text" : "Esto es único en Málaga. A los propietarios les apasiona crear las mejores y más originales tapas.", "source" : "Moniek V. via Google", "url" : "https://www.google.com/maps/contrib/113732323421234179423/place/ChIJd_8ghKP3cg0R5x5rXGr9tJE/@36.7244451,-4.4202563,17z/data=!4m6!1m5!8m4!1e1!2s113732323421234179423!3m1!1e1?hl=es"},
-            {"text" : "Se ha convertido en uno de mis sitios favoritos. Muy acogedor y muy buena atención. Ideal para tomar un vermú.", "source" : "María O. via Google", "url" : "https://www.google.com/maps/contrib/116689621246882345984/place/ChIJd_8ghKP3cg0R5x5rXGr9tJE/@44.0876685,0.3440576,5z/data=!4m6!1m5!8m4!1e1!2s116689621246882345984!3m1!1e1?hl=es"},
-            {"text" : "Lugar muy pintoresco con una muy buena variedad de sabores ricos y saludables. Muy recomendable.", "source" : "Javi C. via Google", "url" : "https://www.google.com/maps/contrib/100582116992476836427/place/ChIJd_8ghKP3cg0R5x5rXGr9tJE/@39.8160002,-6.7148075,7z/data=!4m4!1m3!8m2!1e1!2s100582116992476836427?hl=es"},
-            {"text" : "Pequeño, peculiar, muy amigable y un fantástico ambiente. El mejor lugar para tomar una copa y un bocado o tapas en Málaga.", "source" : "Patrick R. via Facebook", "url" : "https://www.facebook.com/patrick.redmond01/posts/10156556001002856"}
         ]
     };
 
@@ -109,7 +40,7 @@
         'download.drinks' : 'https://drive.google.com/uc?export=download&id=1GgADTtPWS2h-YSEpk7K2hMma5fY5yaec',
         'download.vinos' : 'https://drive.google.com/uc?export=download&id=1Is6l3-UbnuUbXAJHhH6thH8_9wB2eQyL',
         'carousel-images': [
-            'img/carousel_1.jpg',
+            'img/about.jpg',
             'img/carousel_2.jpg',
             'img/carousel_3.jpg',
             'img/carousel_5.jpg',
@@ -355,7 +286,7 @@
             }
 
             $.each(quotes, function (i, obj) {
-                var c = (i % 2) === 0 ? "col-md-4 offset-md-6" : "col-md-4 offset-md-3";
+                var c = (i % 2) === 0 ? "col-md-6 offset-md-8" : "col-md-4 offset-md-3";
 
                 var quote = $("<div/>", {"class": "row"}).append($("<div/>", {
                     "class": c,
