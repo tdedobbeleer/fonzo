2d0c9d591b6bcf88eee1b0437df072b7dd18fa8d
tdedobbeleer <tdedobbeleer@gmail.com>
2022-06-23 16:12:44 +0200

    update opening hours

--- src/js/ui.js
+++ src/js/ui.js
@@ -45,10 +45,10 @@
         'openinghours.monday': [],
         'openinghours.tuesday': [],
         'openinghours.wednesday': [],
-        'openinghours.thursday': [{'open': '19:30', 'closed': '23:00'}],
-        'openinghours.friday': [{'open': '19:30', 'closed': '23:00'}],
-        'openinghours.saturday': [{'open': '19:30', 'closed': '23:00'}],
-        'openinghours.sunday': [{'open': '14:00', 'closed': '16:30'}, {'open': '20:00', 'closed': '23:00'}],
+        'openinghours.thursday': [{'open': '19:00', 'closed': '23:00'}],
+        'openinghours.friday': [{'open': '19:00', 'closed': '23:00'}],
+        'openinghours.saturday': [{'open': '19:00', 'closed': '23:00'}],
+        'openinghours.sunday': [{'open': '13:30', 'closed': '16:30'}, {'open': '19:00', 'closed': '23:00'}],
     };
 
     var DAYS = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
