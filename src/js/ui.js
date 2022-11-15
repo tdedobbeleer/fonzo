c3f37a40032666819fb9f81b36a690169e8027e1
Tom <tdedobbeleer@gmail.com>
2022-11-15 18:23:43 +0100

    in progress

--- src/js/ui.js
+++ src/js/ui.js
@@ -5,6 +5,79 @@
         'nav.home': 'Home',
         'nav.menu': 'Menu',
         'nav.contact': 'Contact & book',
+        'text.title' : 'Fonzo',
+        'text.subtitle' : 'Flavour above everything',
+        'text.modal.qr.title' : 'Discover our flavours',
+        'text.modal.book.title' : 'Book your table',
+        'text.modal.book.body' : '<p>Welcome on the website of Fonzo, a gastronomic restaurant that offers a casual dining concept with sharing plates.</p>' +
+        '<p>Due to the high elaboration of our dishes and small kitchen, we only work with reservations in advance. Each service we only have a few tables available so we can only accept reservations from 2 to 4 persons</p>' +
+        '<p>Please also note that we are not able to take reservations by phone during service! Thank you for understanding!</p>' +
+        '<p>We recommend to book your table a few days in advance if you wish to dine with us. You can choose from the following tables:</p>' +
+        '<p>Thursday till Sunday</p>' +
+        '<p>Dinner: 18h00 - 19h00 - 20h00 - 21h00</p>' +
+        '<p>Sunday lunch<br/>' +
+        '(Only with bookings at least 24h in advance)' +
+        '</p>' +
+        '<p>13h00 - 14h00 - 15h00</p>' +
+        '<p>We only work with reservations so please let us know something at least 24h in advance if you can’t make it!</p>' +
+        '<p>To book, send us a message with <a data-property-href="whatsapp" href="#" rel="nofollow" aria-label="Send a Whatsapp message">WhatsApp</a> or <a data-property-href="signal" href="#" rel="nofollow" aria-label="Send a Signal message">WhatsApp</a> or give us a call (<a class="tel" data-property="tel" data-property-href="tel" href="tel:" rel="nofollow" aria-label="Call us"></a>)</p>' +
+        '<p>Please leave your name, phone number & number of guests.</p>'
+        ,
+        'text.philosophy.title' : 'About',
+        'text.philosophy.subtitle' : 'What do we stand for?',
+        'text.philosophy.bullet.1.title' : 'Fresh',
+        'text.philosophy.bullet.1.body' : 'Our kitchen is built around fresh vegetables, fresh herbs and tasteful spices. We start from pure ' +
+        'ingredients and make almost everything ourselves, always avoiding processed food. We love to work with local and ecological ingredients, top quality ' +
+        'products with great taste.'
+        ,
+        'text.philosophy.bullet.2.title' : 'Dishes to share',
+        'text.philosophy.bullet.2.body' : 'So you can savour a bit of everything and above all, share the tasting experience in the company of your ' +
+        'friends or family. We bring you slow food that takes time to prepare and we also want you to truly appreciate all the flavors so they are served one ' +
+        'dish at a time. We would like to offer you a casual dining experience in Málaga.'
+        ,
+        'text.philosophy.bullet.3.title' : 'Authentic creations',
+        'text.philosophy.bullet.3.body' : 'All of our dishes are made in our little kitchen with love, passion, craft and eye for detail. We enjoy making original ' +
+        'and creative dishes that you will not find elsewhere, using different techniques. We are constantly innovating, ' +
+        'always searching for perfection so our menu is always evolving.'
+        ,
+        'text.philosophy.bullet.4.title' : 'Sustainability',
+        'text.philosophy.bullet.4.body' : 'We are very concerned with the well being of people, animals and our environment. That is why we want to be part of a positive movement and have chosen to serve plant ' +
+        'based creations. We truly believe that what we eat & how we live determines our health & the preservation of our planet for future generations! We don’t compromise on ' +
+        'flavour and create a unique gastronomic plant-based experience to remember.'
+        ,
+        'text.philosophy.bullet.5.title' : 'Quality drinks&nbsp;<i class="icofont-soft-drinks">',
+        'text.philosophy.bullet.5.body' : '<p>"Looking for an aperitif?" Enjoy our selection of Spanish vermuts, sparkling wines, gins or homemade aperitifs. ' +
+        '"Love to wine and dine?" Perfect, just like us! That is why we are always looking for interesting grapes and wines from small bodegas. ' +
+        '"Prefer something without alcohol?" No problem, try our homemade lemonades with fresh fruits or cold brewed iced teas.</p>' +
+        '<p>Be welcome, relax and enjoy our world of flavours.</p>'
+        ,
+        'text.philosophy.footer' : '<p>Everything we serve has been approved by our taste buds!</p><p>Piet & Julie</p>',
+        'text.menu.title' : 'Menu',
+        'text.menu.subtitle' : 'Discover our flavours',
+        'text.menu.dishes' : 'Dishes to share',
+        'text.menu.drinks' : 'Drinks',
+        'text.menu.wine' : 'Wine',
+        'text.monday': "Monday",
+        'text.tuesday': "Tuesday",
+        'text.wednesday': "Wednesday",
+        'text.thursday': "Thursday",
+        'text.friday': "Friday",
+        'text.saturday': "Saturday",
+        'text.sunday': "Sunday",
+        'text.closed': 'Closed',
+        'text.opening.hours': 'Opening hours',
+        'text.quotes' : [
+            {"text" : "What a wonderful and surprising restaurant. After a few days of some average Malaga restaurants we decided to venture slightly further out from the old town and we’re rewarded with a wonderful evening filled with exquisite food, insightful descriptions and ‘one more please!’ cocktails. The care and passion from Piet and Julie is clear in every part of Fonzo. A unique night out and highly, highly recommended.", "source" : "Declan C. via Google", "url" : "https://goo.gl/maps/ereg91Ea9MtQ1RFt9"},
+            {"text" : "Absolutely wonderful on every level - amazing food, delicious wine, gorgeous decor, excellent service. Couldn't fault this place at all! We had the kofta, dumplings, a cous cous stew, and then shared a dessert. It was all exceptional! Both the food and the wine were really fairly priced, so you're getting a lot for your money! If we are ever back in Malaga we will definitely be coming back here.", "source" : "Rachel R. via Google", "url" : "https://goo.gl/maps/67EiEWYL5wzwW4vz9"},
+            {"text" : "Beautifully decorated little restaurant, delicious food make sure to try the lasagne (that's not lasagne) for desert the frozen hummus, never tried vermouth before and unsure which one to have but owner recommended one after a little chat, will definitely be having again. Have to mention the best decorated,and cleanest restroom in malaga. Make sure to book.", "source" : "Margie F. via Google", "url" : "https://goo.gl/maps/cFAgqShVRfozBGZC6"}
+        ]
+    }
+    var LANG_ES = {
+        'nav.philosophy': 'Philosophy',
+        'nav.book': 'Book your table',
+        'nav.home': 'Home',
+        'nav.menu': 'Menu',
+        'nav.contact': 'Contact & book',
         'text.monday': "Monday",
         'text.tuesday': "Tuesday",
         'text.wednesday': "Wednesday",
@@ -63,11 +136,11 @@
     setMap();
     initializeLang();
     moveIfRequested();
-    //initializeLangButtons();
+    initializeLangButtons();
 
     //always init lastly
     setLinks();
-    setPoperties();
+    setProperties();
 
     //show modal
     $('#bookModal').modal('show');
@@ -107,7 +180,7 @@
         });
     }
 
-    function setPoperties() {
+    function setProperties() {
         $("[data-property]").each(function () {
             $(this).text(PROPERTIES[$(this).data('property')]);
         });
@@ -130,10 +203,10 @@
             var l = {};
             if (lang === NL) {
                 l = LANG_NL;
-            } else if (lang === EN) {
-                l = LANG_EN;
-            } else {
+            } else if (lang === ES) {
                 l = LANG_ES;
+            } else {
+                l = LANG_EN;
             }
             $(this).html(getLocalizedMessage($(this).data('translate')));
         });
@@ -169,7 +242,6 @@
     }
 
     function initializeLang() {
-        /**
         var currentLang = getUrlParameter('lang') || localStorage.getItem('lang') || navigator.language || EN;
         if (currentLang.startsWith(EN)) {
             currentLang = EN;
@@ -183,7 +255,6 @@
         }
 
         setLang(currentLang, true);
-         **/
         setLang(EN, true)
     }
 
